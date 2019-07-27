9af4a89e8afd8800e3cd2b231ce7862c7bd1db21
Marc Diaz <marcgabe15@gmail.com>
2019-07-27 15:09:10 -0400

    added Lobby UI

---
+++ LobbyUI.gd
@@ -0,0 +1,34 @@
+extends Control
+
+var RoomID
+var rcbox
+var PC
+var startButton
+var listIndex = 0
+const ListItem = preload("res://ListItem.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	rcbox = get_node('RCBox/RCText')
+	PC = get_node('PC/PCText')
+	startButton = get_node('StartButton')
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func initialize(gameid):
+	rcbox.text = 'Connected to RoomID %s' % gameid
+	
+	
+func addItem(value):
+	var item = ListItem.instance()
+	item.get_node("number").text = str( listIndex )
+	item.get_node("label").text = value
+	item.rect_min_size = Vector2(320,22)
+	$ScrollContainer/list.add_child(item)
+	
+func update_players(players):
+	for player in players:
+		addItem(player['nick'])
+	
