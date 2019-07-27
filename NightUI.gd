5e42389f680219ae21838b974b599777996bac8b
Marc Diaz <marcgabe15@gmail.com>
2019-07-27 17:54:53 -0400

    added night scene

---
+++ NightUI.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+var exfiltrateButton
+var notexfiltrateButton
+var nightlabel
+var time
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	exfiltrateButton = get_node("Panel/Exfiltrate")
+	notexfiltrateButton = get_node("Panel/NotExfiltrate")
+	nightlabel = get_node("Panel/Label")
+	time = get_node("Panel/ColorRect/Label")
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Exfiltrate_pressed():
+	emit_signal("EXFILTRATE_PRESSED") # Replace with function body.
+
+
+func _on_NotExfiltrate_pressed():
+	emit_signal("NOT_EXFILTRATE_PRESSED") # Replace with function body.
