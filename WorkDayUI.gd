edbc268d1c96fd38c428f599a28159ea71bc53c3
Marc Diaz <marcgabe15@gmail.com>
2019-07-27 16:39:10 -0400

    added workday UI

---
+++ WorkDayUI.gd
@@ -0,0 +1,24 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var Header
+var DummyText
+var Time
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Header = get_node('Panel/Header')
+	DummyText = get_node('Panel/DummyText')
+	Time = get_node('Panel/ColorRect/Time')
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _initialize(day, numpeople, playername):
+	Header.text = 'Day %s | Workday' % [str(day)]
+	DummyText.text = 'At 5:00PM %s\n\t> will choose %s people for the night shift' % [str(playername), str(numpeople)]
+	
+func _updateTimer(time):
+	Time.text = time
