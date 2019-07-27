abd061c43874c3ae54db52888f68a392227b97fd
Marc Diaz <marcgabe15@gmail.com>
2019-07-27 13:41:46 -0400

    added signals

--- RoomSelectUI.gd
+++ RoomSelectUI.gd
@@ -16,13 +16,14 @@ func _ready():
 	roomNumber = get_node("RoomNumberLine")
 	joinRoom  = get_node("JoinRoom")
 	createRoom = get_node("CreateRoom")
-	
-	
-	
-func _on_JoinButton_pressed(room_code):
-	if roomNumber.text:
-		emit_signal("JOIN_ROOM", room_code)
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func _on_JoinRoom_pressed():
+	if roomNumber.text:
+		emit_signal("JOIN_ROOM", roomNumber.text)
+
+
+func _on_CreateRoom_pressed():
+	if createRoom.text:
+		emit_signal("CREATE_ROOM")
