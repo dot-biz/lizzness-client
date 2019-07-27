fbe02af9c3ae54db943b59e7bc0f92305701db19
Alexander Shuping <alexandershuping@gmail.com>
2019-07-27 19:08:57 -0400

    get client to waiting-room phase

--- SingleClient.gd
+++ SingleClient.gd
@@ -17,14 +17,12 @@ func create_room(game_name):
 	rpc_id(1, 'create_room', game_name)
 
 remote func create_game_response(pkt):
-	pass
+	print('Received game-create confirmation from server.\n\t> %s' % str(pkt))
 
 remote func join_game_response(pkt):
-	var game_node = Node.new()
-	game_node.set_name('games/%s' % pkt['id'])
-	game_node._initialize(pkt['id'])
-	emit_signal('ROOM_JOINED')
-	# Create /root/games/<id>
-	# call initialize(<id>) on that node
-	# emit ROOM_JOINED signal
-	pass
+	print('Received game-join confirmation from server.\n\t> %s' % str(pkt))
+	var game_node = preload('res://LizznessGame.tscn').instance()
+	game_node.set_name(str(pkt['room_code']))
+	game_node.initialize(pkt['room_code'])
+	get_node('/root/games').add_child(game_node)
+	emit_signal('ROOM_JOINED', pkt['room_code'])
