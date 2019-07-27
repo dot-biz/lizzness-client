7fdc291d995ae1b575c5107b798f9a4aefe9c9c5
Alexander Shuping <alexandershuping@gmail.com>
2019-07-27 18:12:34 -0400

    things

--- RoomSelectUI.gd
+++ RoomSelectUI.gd
@@ -1,28 +1,20 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal JOIN_ROOM
+signal CREATE_ROOM
 
-# Called when the node enters the scene tree for the first time.
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 var roomNumber
 var joinRoom
 var createRoom
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	roomNumber = get_node("Panel/RoomNumberLine")
 	joinRoom  = get_node("Panel/JoinRoom")
 	createRoom = get_node("Panel/CreateRoom")
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	passcc
+
 func _on_JoinRoom_pressed():
 	if roomNumber.text:
 		emit_signal("JOIN_ROOM", roomNumber.text)
-
 
 func _on_CreateRoom_pressed():
 	if createRoom.text:
